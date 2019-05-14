0109009b0cab062004f2413fd52f4f71b5e7af26
Oscar Bernardini <bernardini687@gmail.com>
2019-05-14 12:14:02 +0200

    Add App component

---
+++ src/components/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import FlatList from '../containers/flat_list';
+import Map from '../containers/map';
+
+const App = () => {
+  return (
+    <div>
+      <FlatList />
+      <Map />
+    </div>
+  );
+};
+
+export default App;
