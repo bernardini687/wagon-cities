11279e20fa1a6de2cc1cb3d0fc1a95b120cc28d2
Oscar Bernardini <bernardini687@gmail.com>
2019-05-18 18:59:34 +0200

    Update Map

--- src/containers/map.jsx
+++ src/containers/map.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
 
 const Map = () => {
+  const marker = null;
+  const center = { lat: 48.856614, lng: 2.352222 };
+
   return (
     <div className="col-sm-5" style={{ style: '100vh' }}>
-      DO MAP
+      <GoogleMapReact center={center} defaultZoom={15}>
+        {marker}
+      </GoogleMapReact>
     </div>
   );
 };
