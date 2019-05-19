f87b900896dc59f3a954d1a08382a3ffd97de716
Oscar Bernardini <bernardini687@gmail.com>
2019-05-19 09:35:54 +0200

    Promote Flat to class

--- src/containers/flat.jsx
+++ src/containers/flat.jsx
@@ -2,27 +2,34 @@
 
 import React from 'react';
 
-const Flat = ({ flat }) => {
-  const style = {
-    backgroundImage: `
-      linear-gradient(rgba(0, 0, 0, 0.3),
-      rgba(0, 0, 0, 0.2)),
-      url(${flat.imageUrl})
-    `
-  };
+class Flat extends React.Component {
+  handleClick = () => {
+    //
+  }
 
-  return (
-    <div className="card" style={style}>
-      <div className="card-infos">
-        <h2>{flat.name}</h2>
+  render() {
+    const { flat } = this.props;
+    const style = {
+      backgroundImage: `
+        linear-gradient(rgba(0, 0, 0, 0.3),
+        rgba(0, 0, 0, 0.2)),
+        url(${flat.imageUrl})
+      `
+    };
+
+    return (
+      <div className="card" style={style} onClick={this.handleClick}>
+        <div className="card-infos">
+          <h2>{flat.name}</h2>
+        </div>
+        <div className="card-infos">
+          <h4>
+            {flat.price + ' ' + flat.currency}
+          </h4>
+        </div>
       </div>
-      <div className="card-infos">
-        <h4>
-          {flat.price + ' ' + flat.currency}
-        </h4>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Flat;
