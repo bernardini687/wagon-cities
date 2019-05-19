a774f8ad7b551c242ad6d3c692665ddac9dce2ba
Oscar Bernardini <bernardini687@gmail.com>
2019-05-19 11:42:33 +0200

    Complete test

--- src/containers/map.jsx
+++ src/containers/map.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const Map = () => {
-  const marker = null;
-  const center = { lat: 48.856614, lng: 2.352222 };
+import { connect } from 'react-redux';
+
+const Map = ({ selectedFlat }) => {
+  let marker = null;
+  let center = { lat: 48.856614, lng: 2.352222 };
+
+  if (selectedFlat) {
+    marker = (
+      <div
+        style={{
+          width: "20px",
+          height: "20px",
+          backgroundColor: "red",
+          borderRadius: "50%"
+        }}
+        lat={selectedFlat.lat}
+        lng={selectedFlat.lng}
+      />
+    );
+    center = {
+      lat: selectedFlat.lat,
+      lng: selectedFlat.lng
+    };
+  }
 
   return (
     <div className="col-sm-5" style={{ style: '100vh' }}>
@@ -14,4 +35,8 @@ const Map = () => {
   );
 };
 
-export default Map;
+function mapStateToProps(state) {
+  return { selectedFlat: state.selectedFlat };
+}
+
+export default connect(mapStateToProps)(Map);
