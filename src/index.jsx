1e72972b170c84243846efe7e195db09f8dc553a
Oscar Bernardini <bernardini687@gmail.com>
2019-05-19 09:26:54 +0200

    Add new reducer

--- src/index.jsx
+++ src/index.jsx
@@ -7,11 +7,13 @@ import { createStore, combineReducers } from 'redux';
 import '../assets/stylesheets/application.scss';
 
 import flatsReducer from './reducers/flats_reducer';
+import selectedFlatReducer from './reducers/selected_flat_reducer';
 
 import App from './components/app';
 
 const reducers = combineReducers({
-  flats: flatsReducer
+  flats: flatsReducer,
+  selectedFlat: selectedFlatReducer
 });
 
 const root = document.getElementById('root');
